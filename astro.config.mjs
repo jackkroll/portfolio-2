c2beb8bf1d0334aa54de2850a7552d7ccfa6e626
jack <52892975+jackkroll@users.noreply.github.com>
2026-05-15 15:44:28 -0400

    Build fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
   vite: {
     plugins: [tailwindcss()],
   },
